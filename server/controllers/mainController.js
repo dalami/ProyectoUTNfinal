dfeb36cb2f5fbd1214ebfaee284ae55c1c9f875c
agusmachado <agusmachado@gmail.com>
2023-11-10 09:57:49 -0300

    Actualización de la estructura básica. Modelo-producto y rutas.

---
+++ server/controllers/mainController.js
@@ -0,0 +1,17 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+
+const signup_post = (req, res) =>{}
+const login_post = (req, res) =>{}
+const addProduct_post = (req, res) =>{}
+
+
+
+
+module.exports = {
+    signup_post,
+    login_post,
+    addProduct_post
+}
